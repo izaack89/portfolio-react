07476aa26169c30572d5a6926b317fd53eab7339
German Ramirez <mitsuominagi@gmail.com>
2021-07-29 16:04:57 -0500

    Did change on the bar and I added a function for the toogle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function NavBar({ currentPage, handlePageChange }) {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <a className="navbar-brand" href="#about">German Ramirez</a>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo03">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0  justify-content-end">
-                        <li className="nav-item">
-                        <a
-                        href="#about"
-                        onClick={() => handlePageChange('About')}
-                        className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                        >
-                        About Me
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a
-                        href="#blog"
-                        onClick={() => handlePageChange('Portfolio')}
-                        className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-                        >
-                        Portfolio
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a
-                        href="#contact"
-                        onClick={() => handlePageChange('Contact')}
-                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                        >
-                        Contact
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a
-                        href="#resume"
-                        onClick={() => handlePageChange('Resume')}
-                        className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-                        >
-                        Resume
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-  );
+
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation"  onClick={handleNavCollapse}>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <h2><a className="navbar-brand" href="#about">German Ramirez</a></h2>
+                    <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse justify-content-end`} id="navbarTogglerDemo03">
+                        <ul className="navbar-nav mr-auto mb-2 mb-lg-0 ">
+                            <li className="nav-item">
+                                <a
+                                    href="#about"
+                                    onClick={() => handlePageChange('About')}
+                                    className={currentPage === 'About' ? 'nav-link linkClass linkClassActive' : 'nav-link linkClass'}
+                                >
+                                    About Me
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a
+                                    href="#blog"
+                                    onClick={() => handlePageChange('Portfolio')}
+                                    className={currentPage === 'Portfolio' ? 'nav-link linkClass linkClassActive' : 'nav-link linkClass'}
+                                >
+                                    Portfolio
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a
+                                    href="#contact"
+                                    onClick={() => handlePageChange('Contact')}
+                                    className={currentPage === 'Contact' ? 'nav-link linkClass linkClassActive' : 'nav-link linkClass'}
+                                >
+                                    Contact
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a
+                                    href="#resume"
+                                    onClick={() => handlePageChange('Resume')}
+                                    className={currentPage === 'Resume' ? 'nav-link linkClass linkClassActive' : 'nav-link linkClass'}
+                                >
+                                    Resume
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </header>
+    );
 }
 export default NavBar;
