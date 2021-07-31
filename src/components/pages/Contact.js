6d1f34942b5f24b51eda68d4e5d8b7e04d655fbb
German Ramirez <mitsuominagi@gmail.com>
2021-07-31 17:25:19 -0500

    Added the contact page and all the configuration also added some structured for the projects pages

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,13 @@
 import React, { useState,useEffect } from 'react';
+import axios from 'axios';
 
 export default function Contact() {
 
+    const API_PATH = 'http://rcconsultingmx.com/bootcamp/portfolio-react/emailService.php';
+    const [name_msg, setNameMsg] = useState('');
+    const [email, setEmail] = useState('');
+    const [msg, setMsg] = useState('');
+    const [submitForm, setSubmitForm] = useState(false);
     const [buttonSubmit, setButtonSubmit] = useState(true);
     const [emailValidation, setEmailValidation] = useState(false);
     const [msgValidation, setMsgValidation] = useState(false);
@@ -25,8 +31,7 @@ export default function Contact() {
             if(e.target.value ===""){
                 setError('Error the email cannot be empty!');
                 setEmailValidation(false);
-            }else{           
-                console.log("validae:",validateEmail(e.target.value))     
+            }else{                           
                 if(!validateEmail(e.target.value)){
                     setError('Your email is invalid!');
                     setEmailValidation(false);
@@ -37,6 +42,31 @@ export default function Contact() {
             }
         }
     };
+
+
+    const handleForm = async (e) => {
+        e.preventDefault();
+        setSubmitForm(true);
+        setNameMsg('');
+        setEmail('');
+        setMsg('');
+        setError('');
+        setButtonSubmit(false);
+        axios({
+            method: 'post',
+            url: `${API_PATH}`,
+            data:  {
+                name: name_msg,
+                email: email,
+                msg: msg
+              }
+          })
+            .then(result => {
+                setSubmitForm(true);
+            })
+            .catch(error => setError(error.message));
+    };
+
     useEffect(() => {
         
         if(emailValidation && msgValidation){
@@ -44,27 +74,32 @@ export default function Contact() {
         }else{
             setButtonSubmit(true);
         }
-      });
+      },[emailValidation, msgValidation]);
 
     return (
         <main className="flex-shrink-0">
             <div className="container">
-                <div className="row pt-5 mt-4">
+                <div className="row pt-5 mt-4 pb-5 mb-4">
                     <div className="col-8">
                     <h1 className="textContentH2">Contact</h1>
-                        <form>
+                        {submitForm && (
+                                <div className="col-12 my-3 bg-success text-white p-3">
+                                    Email Sent
+                                </div>
+                        )}
+                        <form onSubmit={handleForm}>
                             <div className="mb-3">
-                                <label for="name" className="form-label">Name</label>
-                                <input type="text" className="form-control" id="name" />
+                                <label className="form-label">Name</label>
+                                <input type="text" className="form-control" id="name" onChange={(event) => setNameMsg(event.target.value)} value={name_msg}/>
                             </div>
                             <div className="mb-3">
-                                <label for="email" className="form-label">Email</label>
-                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onBlur={handleInput} />
+                                <label className="form-label">Email</label>
+                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp"  onChange={(event) => setEmail(event.target.value)}  onBlur={handleInput} value={email}/>
                                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                             </div>
                             <div className="mb-3">
-                                <label for="msg" className="form-label">Message</label>
-                                <textarea class="form-control" id="msg" rows="3" onBlur={handleInput} ></textarea>
+                                <label className="form-label">Message</label>
+                                <textarea className="form-control" id="msg" rows="3"  onChange={(event) => setMsg(event.target.value)}   onBlur={handleInput} value={msg}></textarea>
                             </div>
                             {error && (
                                 <div className="col-12 my-3 bg-danger text-white p-3">
