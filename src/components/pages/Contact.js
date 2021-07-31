e576553d5dc0ae12b971d56e30e42c633f27c64b
German Ramirez <mitsuominagi@gmail.com>
2021-07-31 08:45:05 -0500

    Added the contact me page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,35 +1,34 @@
 import React from 'react';
 
 export default function Contact() {
-  return (
-
-    <main  className="flex-shrink-0">
-        <div className="container-fluid p-0">
-            <h1 className="h3 mb-3 text-center">View Sponsors</h1>
-            <div className="row">
-                <div className="col-12">
-                    <div className="card">
-                        <div className="card-header">
-                            <h5 className="card-title w-100 text-center">
-                                <a href="accounts_add.php">Add New Sponsor</a>
-                            </h5>
-                        </div>
-                        <div className="card-body">
-                            <table id="sponsors-table" className="table table-striped" >
-                                <thead>
-                                    <tr>
-                                        <th>Email</th>
-                                        <th>Name</th>
-                                        <th>Active</th>
-                                        <th>Actions?</th> 
-                                    </tr>
-                                </thead>
-                            </table>
-                        </div>
+    return (
+        <main className="flex-shrink-0">
+            <div className="container">
+                <div className="row pt-5 mt-4">
+                    <div className="col-8">
+                    <h1 className="textContentH2">Contact</h1>
+                        <form>
+                            <div class="mb-3">
+                                <label for="name" className="form-label">Name</label>
+                                <input type="text" className="form-control" id="name" />
+                            </div>
+                            <div className="mb-3">
+                                <label for="email" className="form-label">Email</label>
+                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div className="mb-3">
+                                <label for="msg" className="form-label">Message</label>
+                                <textarea class="form-control" id="msg" rows="3"></textarea>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                    <div className="col-4 align-items-center" style={{alignSelf: "center"}}>                        
+                        <img src={process.env.PUBLIC_URL + '/assets/img/contact.svg'} className="img-fluid rouded" alt="German Ramirez"/> 
                     </div>
                 </div>
-            </div>                        
-        </div>
-    </main>
-  );
+            </div>
+        </main>
+    );
 }
