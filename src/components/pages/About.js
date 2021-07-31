7b64b517ab6b3ae1c19e5b013e7f740b4367d778
German Ramirez <mitsuominagi@gmail.com>
2021-07-31 08:44:18 -0500

    Added the contact me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,9 +4,24 @@ export default function About() {
     return (
         <main className="flex-shrink-0">
             <div className="container">
-                <h1 className="mt-5">Sticky footer with fixed navbar</h1>
-                <p className="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code className="small">padding-top: 60px;</code> on the <code className="small">main &gt; .container</code>.</p>
-                <p>Back to <a href="/docs/5.0/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+                <div className="row pt-5 mt-4">
+                    <div className="col-4">
+                        <img src={process.env.PUBLIC_URL + '/assets/img/german_ramirez.jpg'} className="img-fluid rouded" alt="German Ramirez"/> 
+                    </div>
+                    <div className="col-8">
+                    <h1 className="mt-5 mb-5 textContentH2">Hi, Im German Ramirez</h1>
+                        <p className="lead">
+                        I'm 32 years old and Im living currently on Mexico City.
+
+                        On my free time I like to watch animes, read manga and when the season of the NFL starts I like to watch the games.
+                        My favorite team is the Patriots of New England even that Brady is not there Im still a fan of them. And for the
+                        soccer my team is the America.
+
+                        Other thing that I like to do is to learn new languages of programming and thanks for that I'm able to work with PHP,
+                        Java, React, and many others
+                        </p>
+                    </div>
+                </div>                
             </div>
         </main>
     );
